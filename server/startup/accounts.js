adc039974e5a2040a9e99fc22e4689cedc21ccea
longxuanho <longxuanho@gmail.com>
2016-03-07 22:59:08 +0700

    adding responsive email - done

--- server/startup/accounts.js
+++ server/startup/accounts.js
@@ -1,7 +1,9 @@
 Meteor.startup(function() {
 
     process.env.MAIL_URL = Meteor.settings.private.data.mailgun.key;
+
     SSR.compileTemplate('verifyEmail', Assets.getText('templates/verify-email.html'));
+    SSR.compileTemplate('resetPasswordEmail', Assets.getText('templates/reset-password-email.html'));
 
     // Configures "reset password account" email link
     Accounts.urls.resetPassword = function(token){
@@ -41,15 +43,23 @@ Meteor.startup(function() {
         };
         return SSR.render('verifyEmail', emailData);
     }
+
     // Accounts.emailTemplates.verifyEmail.text = function(user, url) {
     //     return "\n\nChào mừng bạn đến với hành trình Skynet. Cùng nhau, hãy chung tay xây dựng một tương lai mới!" + "\n\nĐể bắt đầu, hãy kích hoạt tài khoản của bạn bằng liên kết dưới đây:\n\n" + url;
     // };
 
     Accounts.emailTemplates.resetPassword.subject = function(user) {
-        return "Thông tin khôi phục mật khẩu từ Skynet"
+        return "Khôi phục mật khẩu của bạn từ Skynet"
     };
-    Accounts.emailTemplates.resetPassword.text = function(user, url) {
-        return "Dear " + user.profile.name + ",\n\nChúng tôi đã nhận được yêu cầu cung cấp mật khẩu mới cho tài khoản của bạn tại Skynet. Nếu bạn không muốn reset lại mật khẩu hoặc không phải là người đưa ra yêu cầu, xin vui lòng bỏ qua email này. Ngược lại, hãy kích hoạt liên kết dưới đây:\n\n" + url;
+    Accounts.emailTemplates.resetPassword.html = function(user, url) {
+        let emailData = {
+            userName: user.profile.name,
+            resetUrl: url
+        };
+        return SSR.render('resetPasswordEmail', emailData);
     };
+    // Accounts.emailTemplates.resetPassword.text = function(user, url) {
+    //     return "Dear " + user.profile.name + ",\n\nChúng tôi đã nhận được yêu cầu cung cấp mật khẩu mới cho tài khoản của bạn tại Skynet. Nếu bạn không muốn reset lại mật khẩu hoặc không phải là người đưa ra yêu cầu, xin vui lòng bỏ qua email này. Ngược lại, hãy kích hoạt liên kết dưới đây:\n\n" + url;
+    // };
 
 });
